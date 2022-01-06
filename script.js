d4e42442c063781c983c4359024790907cb1d028
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2022-01-06 21:52:03 +0530

    Setup the initial conditions.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let number=Math.trunc(Math.random()*6)+1;
+
+// SELECTING ELEMENTS
+const score0=document.querySelector('#score--0');
+const score1=document.getElementById('score--1');
+let diceval=document.querySelector('.dice');
+
+// Setting to initial values
+
+score0.textContent=0;
+score1.textContent=0;
+diceval.style.display='none';
+
