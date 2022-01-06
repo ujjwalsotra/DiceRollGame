519d3a42f4bb712c63ea729d8a105cfab5ef542d
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2022-01-06 22:27:37 +0530

    Added rolled dice function except for case of 1.

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 'use strict';
 
-let number=Math.trunc(Math.random()*6)+1;
-
 // SELECTING ELEMENTS
 const score0=document.querySelector('#score--0');
 const score1=document.getElementById('score--1');
 let diceval=document.querySelector('.dice');
+let btnNew=document.querySelector('.btn--new');
+let btnRoll=document.querySelector('.btn--roll');
+let btnHold=document.querySelector('.btn--hold');
+let current0=document.getElementById('current--0');
 
 // Setting to initial values
 
 score0.textContent=0;
 score1.textContent=0;
-diceval.style.display='none';
+diceval.classList.add('hidden');
 
+let currentScore=0;
+
+//ROLLING DICE FUNCTIONALITY
+btnRoll.addEventListener('click',function(){
+    // Generating a random dice roll
+    const number=Math.trunc(Math.random()*6)+1;
+    // display the dice
+    diceval.classList.remove('hidden');
+    diceval.src=`images/dice-${number}.png`;
+    //check for rolled:1 (if true current ==0 and other player turn)(else roll again)
+    if(number==1)
+    {
+
+    }
+    else
+    {
+       currentScore+=number;
+       current0.textContent=currentScore;
+    }
+})
